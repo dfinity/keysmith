6a082892aec41758632d6b6876a5066364f754f4
Enzo Haussecker <enzo@sovereign.io>
2021-04-08 03:59:09 +0200

    Make all version number components mutable

--- version.go
+++ version.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 )
 
-const (
-	VERSION_CMD = "version"
+const VERSION_CMD = "version"
 
+var (
 	MAJOR = 1
 	MINOR = 2
 	PATCH = 0
+	BUILD = "unknown"
 )
-
-var RELEASE = "unknown"
 
 type VersionCmd struct {
 }
@@ -22,6 +21,6 @@ func NewVersionCmd() *VersionCmd {
 }
 
 func (cmd *VersionCmd) Run() error {
-	fmt.Printf("%d.%d.%d-%s\n", MAJOR, MINOR, PATCH, RELEASE)
+	fmt.Printf("%d.%d.%d-%s\n", MAJOR, MINOR, PATCH, BUILD)
 	return nil
 }
