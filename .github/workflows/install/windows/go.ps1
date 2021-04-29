c7bd0e55c2b079cf382f190dcad99b32c44bab6f
Enzo Haussecker <enzo@sovereign.io>
2021-04-29 23:53:21 +0200

    Fix up go root

--- .github/workflows/install/windows/go.ps1
+++ .github/workflows/install/windows/go.ps1
@@ -5,9 +5,10 @@ $goroot="C:\go"
 $gopath="$home\go"
 Push-Location $env:TEMP
 $client = new-object System.Net.WebClient
-$client.DownloadFile("https://storage.googleapis.com/golang/$zipfile", "$zipfile")
+$client.DownloadFile("https://storage.googleapis.com/golang/$zipfile", $zipfile)
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory("$zipfile", "$goroot")
+System.IO.Compression.ZipFile.ExtractToDirectory($zipfile, $pwd)
+Move-Item -Path go -Destination $goroot
 Pop-Location
 echo "GOROOT=$goroot" >> $env:GITHUB_ENV
 echo "GOPATH=$gopath" >> $env:GITHUB_ENV
