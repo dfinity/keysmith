4b55ebc9574d0746f2f6c92f3d88af07a5b16c80
Enzo Haussecker <enzo@sovereign.io>
2021-04-29 23:18:48 +0200

    Correct usage of home environment variable

--- .github/workflows/install/windows/go.ps1
+++ .github/workflows/install/windows/go.ps1
@@ -2,7 +2,7 @@ $arch="amd64"
 $version="1.16"
 $zipfile="go$version.windows-$arch.zip"
 $goroot="C:\go"
-$gopath="$env:HOME/go"
+$gopath="$env:HOMEPATH/go"
 Push-Location $env:TEMP
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://storage.googleapis.com/golang/$zipfile", "$zipfile")
@@ -13,4 +13,4 @@ echo "GOROOT=$goroot" >> $env:GITHUB_ENV
 echo "GOPATH=$gopath" >> $env:GITHUB_ENV
 echo "$goroot/bin" >> $env:GITHUB_PATH
 echo "$gopath/bin" >> $env:GITHUB_PATH
-New-Item -ItemType "directory" -Name "go" -Path $env:HOME
+New-Item -ItemType "directory" -Name "go" -Path $env:HOMEPATH
