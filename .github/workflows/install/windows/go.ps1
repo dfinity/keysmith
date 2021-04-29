9c78be65ad0de42c96952e2047dfc946b5dd6dd8
Enzo Haussecker <enzo@sovereign.io>
2021-04-30 00:51:21 +0200

    Clean up and document Go install script for Windows

--- .github/workflows/install/windows/go.ps1
+++ .github/workflows/install/windows/go.ps1
@@ -1,15 +1,26 @@
-$arch="amd64"
+#!/usr/bin/env pwsh
+
+# Define operating system and architecture.
+$goos="windows"
+$goarch="amd64"
+
+# Define version and zip file.
 $version="1.16"
-$zipfile="go$version.windows-$arch.zip"
-$goroot="C:\go"
-$gopath="$home\go"
+$zipfile="go$version.$goos-$goarch.zip"
+
+# Install Go compiler.
 Push-Location $env:TEMP
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://storage.googleapis.com/golang/$zipfile", $zipfile)
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $pwd)
-Move-Item -Path go -Destination $goroot
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, "C:\")
 Pop-Location
+
+# Configure workspace.
+$goroot="C:\go"
+$gopath="$home\go"
+echo "GOOS=$goos" >> $env:GITHUB_ENV
+echo "GOARCH=$goarch" >> $env:GITHUB_ENV
 echo "GOROOT=$goroot" >> $env:GITHUB_ENV
 echo "GOPATH=$gopath" >> $env:GITHUB_ENV
 echo "$goroot/bin" >> $env:GITHUB_PATH
