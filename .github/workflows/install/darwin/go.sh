417f822afbd38c63eb9e12bfde34ee87b88f8a9f
Enzo Haussecker <enzo@sovereign.io>
2021-04-30 00:17:08 +0200

    Clean up Go installation on Darwin

--- .github/workflows/install/darwin/go.sh
+++ .github/workflows/install/darwin/go.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 set -ex
-ARCH="$(uname -m)"
+
+# Define operating system and architecture.
+GOOS=darwin
+case "$(uname -m)" in
+    x86_64)
+        GOARCH="amd64";;
+    arm64)
+        GOARCH="arm64";;
+    *)
+        echo "Error: Unsupported architecture!"
+        exit 1
+esac
+
+# Define version and tarball. 
 VERSION="1.16"
-TARBALL="go${VERSION}.darwin-${ARCH}.tar.gz"
+TARBALL="go${VERSION}.${GOOS}-${GOARCH}.tar.gz"
+
+# Install Go compiler.
 pushd /tmp
 curl -L -O -s "https://storage.googleapis.com/golang/${TARBALL}"
 pushd /usr/local
 sudo tar -f /tmp/${TARBALL} -x
 popd
 popd
+
+# Configure workspace.
 GOROOT="/usr/local/go"
 GOPATH="${HOME}/go"
+echo "GOOS=${GOOS}" >> ${GITHUB_ENV}
+echo "GOARCH=${GOARCH}" >> ${GITHUB_ENV}
 echo "GOROOT=${GOROOT}" >> ${GITHUB_ENV}
 echo "GOPATH=${GOPATH}" >> ${GITHUB_ENV}
 echo "${GOROOT}/bin" >> ${GITHUB_PATH}
