5bb14c6c79931566456c013add85c70a3297996f
kevin <q964049459@gmail.com>
2021-05-05 00:16:57 +0800

    net service init

--- netservice/netservice.go
+++ netservice/netservice.go
@@ -1,3 +1,25 @@
-package net
+package netservice
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 const NETWORK_CMD = "netservice"
+
+const NETWORK_CMD_START = "start"
+
+const NETWORK_CMD_STOP = "stop"
+
+const NETWORK_CMD_RESTART = "restart"
+
+var router = gin.Default()
+
+func start() {
+
+	router.GET("/hello/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+	router.Run(":8081") // listen and serve on 0.0.0.0:8081
+}
